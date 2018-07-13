27387e2a350dde185e2d8de3e6e1430e2d70659c
Wikström Andreas <andreas.wikstrom@scania.com>
2018-07-13 14:57:25 +0200

    Initial commit - Added project structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+
+declare var CorporateUi: any;
 
 @Component({
-  selector: 'app-root',
+  selector: '#cool-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  siteName = 'Cool App';
+  items:any = [{ name: 'home' }];
+  apa:string = 'nav-item';
+
+  constructor() {
+    CorporateUi.baseComponents();
+    this.addItem({ name: 'contact' })
+  }
+
+  addItem(item: any) {
+    this.items.push(item)
+  }
 }
